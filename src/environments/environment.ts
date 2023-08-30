980444186b36f9dee4206ad6a3e6d8ff28fa59b5
FGacheru <gacheruf12@gmail.com>
2023-08-30 10:43:27 +0300

    Initial Commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'mosh-demo-254',
+    appId: '1:64986303786:web:7e63a7776fa4b4bb2de253',
+    databaseURL: 'https://mosh-demo-254-default-rtdb.firebaseio.com',
+    storageBucket: 'mosh-demo-254.appspot.com',
+    apiKey: 'AIzaSyAZYk40O9sFBxSqML0iDIp83bE-b_fCguw',
+    authDomain: 'mosh-demo-254.firebaseapp.com',
+    messagingSenderId: '64986303786',
+    measurementId: 'G-V60GYVR922',
+  }
+}
